1bdf43f013b804dccb86725aac4f4152454ef5af
root <bruno.colombini@me.com>
2016-07-24 18:42:35 +0100

    First commit

---
+++ install_noip.bash
@@ -0,0 +1,7 @@
+cd /usr/local/src/
+sudo wget http://www.no-ip.com/client/linux/noip-duc-linux.tar.gz
+sudo tar xf noip-duc-linux.tar.gz
+cd noip-2.1.9-1/
+sudo make install
+sudo chmod 700 /usr/local/bin/noip2
+
