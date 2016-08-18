9ae3652a97756e5aff71609ad3cbd3ca8be2dd03
Bruno Colombini <bruno.colombini@me.com>
2016-08-18 11:28:12 -0300

    Update install_noip.bash

--- install_noip.bash
+++ install_noip.bash
@@ -3,5 +3,5 @@ sudo wget http://www.no-ip.com/client/linux/noip-duc-linux.tar.gz
 sudo tar xf noip-duc-linux.tar.gz
 cd noip-2.1.9-1/
 sudo make install
-sudo chmod 700 /usr/local/bin/noip2
+sudo chmod 777 /usr/local/bin/noip2
 sudo apt-get install ssh
